0d13f005512b738e9182e432166402527be436dd
gabsmelin <rm550202@fiap.com.br>
2024-07-05 16:25:52 -0300

    Página de cadastro finalizada, faltando linkar api do back-end.

--- src/page/cadastro/App.jsx
+++ src/page/cadastro/App.jsx
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo-edc.png';
+import defaultImage from '../../assets/img/addFoto.png'
+import './App.css';
+
+
 
 export default function Home() {
-  const [imagem, setImagem] = useState(null); 
+  const [nome, setNome] = useState('');
+  const [dataNascimento, setDataNascimento] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [tipo, setTipo] = useState('');
+  const [cep, setCep] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [estado, setEstado] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [rua, setRua] = useState('');
+  const [numero, setNumero] = useState('');
+  const [complemento, setComplemento] = useState('');
+  const [imagem, setImagem] = useState(null);
 
   const uploadImage = async (e) => {
     e.preventDefault();
@@ -19,7 +35,7 @@ export default function Home() {
   return (
     <div className="container">
       <header className="home-header">
-        <img src={logo} alt="" />
+        <img src={logo} alt="Logo" />
       </header>
 
       <div className="home-corpo">
@@ -33,19 +49,149 @@ export default function Home() {
           <button><Link className='link-style' to='/addCurso'>Adicionar Curso</Link></button>
         </div>
 
-        
-        <form onSubmit={uploadImage}>
-          <label htmlFor="image">Imagem:</label>
-          <input type="file" id="image" name="image" onChange={handleFileChange} />
-          <button type="submit">Salvar</button>
-        </form>
-
-        {imagem && (
-          <div>
-            <h2>Imagem:</h2>
-            <img src={imagem} alt="Imagem carregada" style={{ maxWidth: '100%' }} />
+        <p>Dados do Aluno</p>
+        <form onSubmit={uploadImage} className='formulario'>
+          <div className='input-aluno'>
+            <div className='input'>
+              <label htmlFor="nome">Nome:</label>
+              <input 
+                type="text" 
+                id="nome" 
+                name="nome" 
+                value={nome} 
+                onChange={(e) => setNome(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="dataNascimento">Data de Nascimento:</label>
+              <input 
+                type="date" 
+                id="dataNascimento" 
+                name="dataNascimento" 
+                value={dataNascimento} 
+                onChange={(e) => setDataNascimento(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="email">Email:</label>
+              <input 
+                type="text" 
+                id="email" 
+                name="email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="telefone">Telefone:</label>
+              <input 
+                type="text" 
+                id="telefone" 
+                name="telefone" 
+                value={telefone} 
+                onChange={(e) => setTelefone(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="tipo">Tipo:</label>
+              <input 
+                type="text" 
+                id="tipo" 
+                name="tipo" 
+                value={tipo} 
+                onChange={(e) => setTipo(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="cep">CEP:</label>
+              <input 
+                type="text" 
+                id="cep" 
+                name="cep" 
+                value={cep} 
+                onChange={(e) => setCep(e.target.value)} 
+              />
+            </div>
           </div>
-        )}
+
+          <div className='input-aluno'>
+            <div className='input'>
+              <label htmlFor="cidade">Cidade:</label>
+              <input 
+                type="text" 
+                id="cidade" 
+                name="cidade" 
+                value={cidade} 
+                onChange={(e) => setCidade(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="estado">Estado:</label>
+              <input 
+                type="text" 
+                id="estado" 
+                name="estado" 
+                value={estado} 
+                onChange={(e) => setEstado(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="bairro">Bairro:</label>
+              <input 
+                type="text" 
+                id="bairro" 
+                name="bairro" 
+                value={bairro} 
+                onChange={(e) => setBairro(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="rua">Rua:</label>
+              <input 
+                type="text" 
+                id="rua" 
+                name="rua" 
+                value={rua} 
+                onChange={(e) => setRua(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="numero">Número:</label>
+              <input 
+                type="text" 
+                id="numero" 
+                name="numero" 
+                value={numero} 
+                onChange={(e) => setNumero(e.target.value)} 
+              />
+            </div>
+            <div className='input'>
+              <label htmlFor="complemento">Complemento:</label>
+              <input 
+                type="text" 
+                id="complemento" 
+                name="complemento" 
+                value={complemento} 
+                onChange={(e) => setComplemento(e.target.value)} 
+              />
+            </div>
+          </div>
+
+          <div className='input-imagem'>
+            <label htmlFor="imagem">Foto:</label>
+            <label className='custom-file-upload'>
+              <input type="file" id="imagem" name="imagem" onChange={handleFileChange} />
+              Escolher arquivo
+            </label>
+            <div>
+              <img 
+                className="uploaded-image" 
+                src={imagem || defaultImage} 
+                alt="Imagem carregada" 
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
