430afc71a797204b7e3bca4da6e3d460b70f8147
gabsmelin <rm550202@fiap.com.br>
2024-07-16 21:07:56 -0300

    Adicionando rotas necessárias.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './page/home/App.jsx';
 import Cadastrar from './page/cadastro/App.jsx';
+import Detalhes from './page/detalhes/App.jsx';
+import Editar from './page/editar/App.jsx';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/cadastrar" Component={Cadastrar} />
+        <Route path="/detalhes" Component={Detalhes} />
+        <Route path="/editar" Component={Editar} />
       </Routes>
     </Router>
   );
